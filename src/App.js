96750ea8d320efebc31a1f57a85c8c282b560f6f
godwin <godwin.kalu@senior.co.uk>
2021-08-29 09:52:45 +0100

    bootstraps frontend, creates needed pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Form, Button } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Login from "./components/Login";
+import Register from "./components/Register";
+import DashBoard from "./components/Dashboard";
+import AuthRoute from "./components/AuthRoute";
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <BrowserRouter>
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+          <AuthRoute path={["/", "/dashboard"]} component={DashBoard} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+
---
+++ src/components/Dashboard.js
@@ -0,0 +1,14 @@
+
+
+const Dashboard = () => {
+    return (
+        <div className="App">
+            <header className="App-header">
+                Dashboard
+            </header>
+
+        </div>
+    );
+}
+
+export default Dashboard;
